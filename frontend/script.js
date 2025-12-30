88931e8d328d975bcee7b87819e8cc88d8918c72
Sparsh Kumar <asparsh.professional@gmail.com>
2025-12-30 12:07:26 +0530

    shop module is in development now!

--- frontend/script.js
+++ frontend/script.js
@@ -13,3 +13,19 @@ if (close) {
         navbar.classList.remove('active');
     });
 }
+
+
+
+const images = [
+  "img/banner1.jpg",
+  "img/banner2.jpg",
+  "img/banner3.jpg"
+];
+
+let index = 0;
+const hero = document.querySelector(".hero-img");
+
+setInterval(() => {
+  index = (index + 1) % images.length;
+  hero.src = images[index];
+}, 3000);
